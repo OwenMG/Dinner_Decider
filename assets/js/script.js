d4c8f0cfb7a590de6ff0fb44ed3085dcf583ace3
OwenMG <36777755+OwenMG@users.noreply.github.com>
2022-04-04 21:43:25 -0700

    adds comments explaining food API code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 var foodAPIUrl = "https://api.spoonacular.com/recipes/complexSearch";
+// user input variable, manual set for now for testing purposes
 var userRecipeInput = "beef wellington"
-
+// fetching url with user input as query
     fetch(foodAPIUrl+"?query="+userRecipeInput+"&number=1&fillIngredients=true&apiKey=cd67472648f34dd6a33c096e8313fcea")
     .then(function (response) {
         if(response.ok) {
             console.log(response)
             response.json().then(function(data) {
                 console.log(data);
+                // creating a "result" object, adding the recipe title, and an array to be filled with ingredients
                 var recipeResult = {
                     title: data.results[0].title,
                     ingredients: [],
                 };
+                // for each ingredient in the result, adds it to the array in our response object
                 for (i=0; i<data.results[0].missedIngredients.length; i++){
                     recipeResult.ingredients.push(data.results[0].missedIngredients[i].original);
                     
