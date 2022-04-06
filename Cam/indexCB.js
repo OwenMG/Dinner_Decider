877c41ab9f6345128b881a583c1baa2e87a7f170
Cameron Buss <camnb97@gmail.com>
2022-04-05 17:58:25 -0700

    styled modal, made more js connects

--- Cam/indexCB.js
+++ Cam/indexCB.js
@@ -1,8 +1,10 @@
 //modal
 
-  let allergyBtn = document.querySelector("#allergy");
-  let modalBg = document.querySelector('.modal-background');
-  let modal = document.querySelector('.modal')
+let allergyBtn = document.querySelector("#allergy-modal");
+let modalBg = document.querySelector('.modal-background');
+let modal = document.querySelector('.modal')
+let closeModal = document.querySelector("#close-btn")
+let saveAllergies = document.querySelector("#save-allergies")
 
   allergyBtn.addEventListener('click', () => {
       modal.classList.add('is-active');
@@ -10,4 +12,14 @@
 
   modalBg.addEventListener('click', () => {
       modal.classList.remove('is-active');
+
   });
+
+closeModal.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+})
+  
+saveAllergies.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+
+})
